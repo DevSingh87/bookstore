baa47dd9e9464eba50907e39fa7e65005f082351
TechieDev <techiedev1987@gmail.com>
2016-01-06 23:05:24 +0530

    Author module completed

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,4 +7,46 @@ class AuthorsController < ApplicationController
 		@author = Author.find(params[:id])
         #require 'pry'; binding.pry
     end
+
+    def new
+    	@author = Author.new
+    end
+
+    def edit
+    	 @author = Author.find(params[:id])
+    end
+
+    def create
+       #require 'pry'; binding.pry
+       @author = Author.create(author_params)
+       if @author.save
+       	flash[:success] = "Author has been created"
+         redirect_to author_path(@author)
+       else
+       	flash[:alert] = "Author has not been created"
+         render :new
+       end
+    end 
+    def update
+    	@author = Author.find(params[:id])
+    	if @author.update_attributes(author_params)
+        	flash[:success] = "Author has been updated"
+    	   redirect_to @author
+        else
+        	flash[:alert]  = "Author has not be updated"
+        	render :edit
+        end
+    end
+
+    def destroy
+    @author = Author.find(params[:id])
+    if @author.destroy
+      flash[:success] = "Author has been deleted."
+      redirect_to authors_path
+    end
+  end
+    private 
+    def author_params
+    	params.require(:author).permit(:first_name, :last_name)
+    end
 end
--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -24,5 +24,113 @@ describe AuthorsController do
 			expect(response).to render_template :show
 		end
 	end
+	describe "GET #new" do
+		it "assigns new object to the @author varaible" do
+			get :new
+			expect(assigns(:author)).to be_instance_of(Author)
+
+		end
+
+		it "renders new view file" do
+			get :new
+			expect(response).to render_template :new
+		end
+	end
+	describe "POST #create" do
+		context " a successful creation of an author" do
+			it "creates the new author object" do
+			  post :create, author: FactoryGirl.attributes_for(:author)
+			  expect(Author.count).to eq(1)
+			end
+			it "redirects to the show page" do
+			  post :create, author: FactoryGirl.attributes_for(:author)
+			  expect(response).to redirect_to author_path(Author.first)
+		    end
+			it "sets the success flash message" do
+				post :create, author: FactoryGirl.attributes_for(:author)
+			    expect(flash[:success]).to be_present
+			end
+		end
+		context " an unsuccessful creation of an author" do
+			it "doesn't create an author" do
+			  post :create, author: FactoryGirl.attributes_for(:author, first_name: nil)
+			  expect(Author.count).to eq(0)
+			end
+			it "renders new template" do
+			  post :create, author: FactoryGirl.attributes_for(:author, first_name: nil)
+			  expect(response).to render_template :new
+			end
+			it "sets an error message" do 
+			  post :create, author: FactoryGirl.attributes_for(:author, first_name: nil)
+			  expect(flash[:alert]).to be_present
+			end
+
+		end
+	end
+
+	describe "GET #edit" do
+	   let(:author){FactoryGirl.create(:author)}
+       it "finds the author with the given id and assigns it to the @author variable" do
+       	get :edit, id: author
+       	expect(assigns(:author)).to eq(author)
+
+       end
+       it "renders the edit template" do
+       	get :edit, id: author
+       	expect(response).to render_template :edit
+       end
+	end
+
+	describe "PUT #update" do
+		
+		 context "author gets updated successfully" do
+		 	let(:author){FactoryGirl.create(:author)}
+		 	it "updates the author object" do
+		 	  put :update, author: FactoryGirl.attributes_for(:author, first_name: 'Dev'), id: author
+		 	  expect(Author.first.first_name).to eq('Dev')
+		 	end
+		 	it "redirects to the show page" do
+		 		put :update, author: FactoryGirl.attributes_for(:author, first_name: 'Dev'), id: author
+		 		expect(response).to redirect_to author_path(Author.first)
+		 	end
+		 	it "flashes the success message" do
+		 		put :update, author: FactoryGirl.attributes_for(:author, first_name: 'Dev'), id: author
+		 		expect(flash[:success]).to be_present
+		 	end
+		 end
+		 context "author does not get updated" do
+		 	let(:author){FactoryGirl.create(:author, first_name: 'Dev')}
+		 	it "does not update the author object" do
+		 	  put :update, author: FactoryGirl.attributes_for(:author, first_name: nil), id: author
+		 	  expect(Author.first.first_name).to eq('Dev')
+		 	end
+		 	it "renders the edit page" do
+		 		put :update, author: FactoryGirl.attributes_for(:author, first_name: ''), id: author
+		 		expect(response).to render_template :edit
+		 	end
+		 	it "flashes the alert message" do
+		 		put :update, author: FactoryGirl.attributes_for(:author, first_name: ''), id: author
+		 		expect(flash[:alert]).to be_present
+		 	end
+		 end
+	end
+
+	describe "DELETE #destroy" do
+		let(:author){FactoryGirl.create(:author)}
+		it "deltes the author with the given id" do
+			delete :destroy, id: author
+			expect(Author.count).to eq(0)
+		end
+
+		it "flashes the success message" do
+			delete :destroy, id: author
+			expect(flash[:success]).to be_present
+		end
+
+		it "redirects to the index page" do
+			delete :destroy, id: author
+			expect(response).to redirect_to authors_path
+		end
+	end
 	
 end
